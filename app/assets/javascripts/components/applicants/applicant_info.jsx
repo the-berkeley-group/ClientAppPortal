9102f4ea1d6e86c69feeeb1641d9489a74728395
Adithya Iyengar <adithyaiyengar97@gmail.com>
2016-06-15 09:03:02 -0400

    Kept appropriate form fields, need to fix disappearing info on save.

--- app/assets/javascripts/components/applicants/applicant_info.jsx
+++ app/assets/javascripts/components/applicants/applicant_info.jsx
@@ -20,10 +20,6 @@ class ApplicantInfo extends React.Component {
                 <h2 className="category-title">{CategoryTitles.PERSONAL}</h2>
                 { this._renderInput("first_name", "First Name") }
                 { this._renderInput("last_name", "Last Name") }
-                { this._renderInput("year", "Year") }
-                { this._renderInput("major", "Major") }
-                { this._renderInput("gpa", "GPA") }
-                { this._renderInput("units", "Spring 2016 Units") }
                 { this._renderInput("phone", "Phone Number") }
                 { this._renderInput("org", "Organization")}
                 { this._renderInput("address", "Address")}
